fbd1ea38798d19eca63e2e9130c3b6e1926cecb3
dhruvakhariaa <vakhariadhruv526@gmail.com>
2025-01-09 08:51:41 +0530

    Using ` feature in js

---
+++ Day-7/demo.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+var a = 10
+var b = 20
+// console.log(a+b)
+
+console.log(`The sum of ${a} and ${b} is ${a+b}`)
+
+console.log(`The sum of ${a} 
+            and ${b} is     
+                        ${a+b}`)
+
+if(a > 10){
+    console.log("a is greater than 10")
+}else{
+    console.log("b is greater than 10")
+}
